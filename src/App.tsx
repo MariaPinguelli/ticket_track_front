8fe3373f5048e06d39c6594880d9cd82c94fb599
lbritors <britoleticiars@gmail.com>
2024-04-15 20:55:11 -0300

    add register component and layout

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import RegisterPage from "./pages/RegisterPage"
+
+
+
+export default function App() {
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<RegisterPage/>} />
+
+      </Routes>
+    
+    
+    
+    </BrowserRouter>
+  )
+}
+
+
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import LogoImage from '../assets/ticket-logo.png'
+
+export default function RegisterPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
+
+  function handleRegister(e: { preventDefault: () => void; }) {
+    e.preventDefault();
+    if (password.length < 6 || password !== confirm) {
+      alert('Senha inválida!');
+      return
+    }
+
+    
+  }
+
+  return (
+    <Container>
+      <Logo src={LogoImage}></Logo>
+      <Form onSubmit={handleRegister}>
+        <input
+          placeholder='Nome'
+          type='text'
+          required
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <input
+          placeholder='Email'
+          type='email'
+          required
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input
+          placeholder='Senha'
+          type='password'
+          required
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <input
+          placeholder='Confirme a senha'
+          type='password'
+          required
+          value={confirm}
+          onChange={e => setConfirm(e.target.value)}
+        />
+        <button type='submit'>Cadrastrar</button>
+      </Form>
+
+    </Container>
+  )
+}
+
+
+const Container = styled.div`
+display: flex;
+height: 100vh;
+width: 100%;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`
+const Logo = styled.img`
+height: 150px;
+width: 160  px;
+
+
+`
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+gap: 10px;
+
+  input{
+    border-radius: 7px;
+    border: 1px solid #D5D7CC;
+    height: 25px;
+    width: 200px;
+  }
+
+  button{
+    border-radius: 8px;
+    background-color: #7ab19c;
+    color: #2F2F2F;
+    font-size: 15px;
+    font-weight: 700;
+    border: 1px solid #D5D7CC;
+    height: 30px;
+    width: 200px;
+  }
+`
